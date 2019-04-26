b6025e40dfbf20e0c674a983b3d719e6ccdd2531
Mario Werner <mario.werner@iaik.tugraz.at>
2019-04-26 17:33:30 +0200

    fixed another deployment bug
    Additionally, the pypi helper script has been renamed for
    consistency and the version has been bumped again.

--- ConanTools/__init__.py
+++ ConanTools/__init__.py
@@ -1,8 +1,11 @@
 import string
+from typing import Optional
 
 
 # https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
-def slug(input: str) -> str:
+def slug(input: Optional[str]) -> Optional[str]:
+    if input is None:
+        return None
     whitelist = set(string.ascii_letters + string.digits)
 
     def sanitize_char(ch):
